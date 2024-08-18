b709d599276c5be4390ccbf10b507bb5d14729f2
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-19 03:10:24 +0530

    created our own native module react-native-termux

---
+++ modules/react-native-termux/android/src/main/java/com/termux/ShellPackage.kt
@@ -0,0 +1,17 @@
+package com.termux
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class ShellPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(ShellModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
+
